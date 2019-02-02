25739ce00574ab2010b465aaf19a898a9ab03006
instabledesign <depotdesign@gmail.com>
2019-02-02 13:11:04 +0100

    implement Syslog handler (#8)
    * Implement syslog handler

---
+++ handler/syslog_test.go
@@ -0,0 +1,106 @@
+package handler_test
+
+import (
+	"bou.ke/monkey"
+	"github.com/gol4ng/logger"
+	"github.com/gol4ng/logger/handler"
+	"github.com/gol4ng/logger/mocks"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"log/syslog"
+	"reflect"
+	"testing"
+)
+
+func TestSyslog_HandleWithError(t *testing.T) {
+	var w *syslog.Writer // Has to be a pointer to because `Dial` has a pointer receiver
+
+	monkey.Patch(syslog.Dial, func(network, raddr string, priority syslog.Priority, tag string) (*syslog.Writer, error) {
+		assert.Equal(t, "fake_network", network)
+		assert.Equal(t, "fake_raddr", raddr)
+		assert.Equal(t, syslog.LOG_DEBUG, priority)
+		assert.Equal(t, "fake_tag", tag)
+		return w, nil
+	})
+	monkey.PatchInstanceMethod(reflect.TypeOf(w), "Write", func(_ *syslog.Writer, m []byte) (int, error) {
+		assert.Equal(t, []byte("fake_syslog_message"), m)
+		return 0, errors.New("fake_syslog_write_error")
+	})
+	defer monkey.UnpatchAll()
+
+	logEntry := logger.Entry{Level: -1}
+
+	mockFormatter := mocks.FormatterInterface{}
+	mockFormatter.On("Format", logEntry).Return("fake_syslog_message")
+
+	h, _ := handler.NewSyslog(&mockFormatter, "fake_network", "fake_raddr", syslog.LOG_DEBUG, "fake_tag")
+
+	assert.EqualError(t, h.Handle(logEntry), "fake_syslog_write_error")
+}
+
+func TestSyslog_Handle(t *testing.T) {
+	syslogMethods := map[logger.Level]string{
+		logger.DebugLevel:     "Debug",
+		logger.NoticeLevel:    "Notice",
+		logger.InfoLevel:      "Info",
+		logger.WarningLevel:   "Warning",
+		logger.ErrorLevel:     "Err",
+		logger.AlertLevel:     "Alert",
+		logger.CriticalLevel:  "Crit",
+		logger.EmergencyLevel: "Emerg",
+	}
+
+	tests := []struct {
+		name             string
+		level            logger.Level
+		methodToBeCalled string
+	}{
+		{level: logger.DebugLevel},
+		{level: logger.InfoLevel},
+		{level: logger.NoticeLevel},
+		{level: logger.WarningLevel},
+		{level: logger.ErrorLevel},
+		{level: logger.AlertLevel},
+		{level: logger.CriticalLevel},
+		{level: logger.EmergencyLevel},
+	}
+	for _, test := range tests {
+		t.Run("test syslog "+test.level.String(), func(t *testing.T) {
+			syslogMsg := "this is a test " + test.level.String() + " syslog message"
+			var w *syslog.Writer // Has to be a pointer to because `Dial` has a pointer receiver
+
+			monkey.Patch(syslog.Dial, func(network, raddr string, priority syslog.Priority, tag string) (*syslog.Writer, error) {
+				assert.Equal(t, "fake_network", network)
+				assert.Equal(t, "fake_raddr", raddr)
+				assert.Equal(t, syslog.LOG_DEBUG, priority)
+				assert.Equal(t, "golang", tag)
+				return w, nil
+			})
+
+			for l, syslogMethod := range syslogMethods {
+				if test.level != l {
+					monkey.PatchInstanceMethod(reflect.TypeOf(w), syslogMethod, func(_ *syslog.Writer, m string) error {
+						t.Fatal("method syslog.Writer::" + syslogMethod + " was not expected to be called")
+						return nil
+					})
+					continue
+				}
+				monkey.PatchInstanceMethod(reflect.TypeOf(w), syslogMethod, func(_ *syslog.Writer, m string) error {
+					assert.Equal(t, syslogMsg, m)
+					return nil
+				})
+			}
+			defer monkey.UnpatchAll()
+
+			//LOG DEBUG HERE
+			logEntry := logger.Entry{Level: test.level}
+
+			mockFormatter := mocks.FormatterInterface{}
+			mockFormatter.On("Format", logEntry).Return(syslogMsg)
+
+			h, _ := handler.NewSyslog(&mockFormatter, "fake_network", "fake_raddr", syslog.LOG_DEBUG, "")
+
+			assert.Nil(t, h.Handle(logEntry))
+		})
+	}
+}
