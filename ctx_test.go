d52198cdd1b173b8c4e8d2fa9682f15a64c471f6
Quentin Neyrat <qneyrat@eleven-labs.com>
2019-03-18 22:32:40 +0100

    fix test

--- ctx_test.go
+++ ctx_test.go
@@ -8,22 +8,27 @@ import (
 	"github.com/gol4ng/logger"
 )
 
-func TestCtx(t *testing.T) {
+func Test_WithContext_New(t *testing.T) {
 	log := logger.NewLogger(logger.NewNopHandler())
 	ctx := log.WithContext(context.Background())
 	log2 := logger.Ctx(ctx)
 	if !reflect.DeepEqual(log, log2) {
 		t.Error("Ctx did not return the expected logger")
 	}
+}
 
-	ctx = log.WithContext(ctx)
-	log2 = logger.Ctx(ctx)
+func Test_WithContext_AlreadyHave(t *testing.T) {
+	log := logger.NewLogger(logger.NewNopHandler())
+	ctx := log.WithContext(log.WithContext(context.Background()))
+	log2 := logger.Ctx(ctx)
 	if !reflect.DeepEqual(log, log2) {
 		t.Error("Ctx did not return the expected logger")
 	}
+}
 
-	log2 = logger.Ctx(context.Background())
-	if !reflect.DeepEqual(logger.NewNopLogger(), log2) {
+func Test_Ctx_Empty(t *testing.T) {
+	log := logger.Ctx(context.Background())
+	if !reflect.DeepEqual(logger.NewNopLogger(), log) {
 		t.Error("Ctx did not return the expected logger")
 	}
 }
