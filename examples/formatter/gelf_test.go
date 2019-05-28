caca5af0d8866ada3b1325a8912e09ee24dbdad0
Anthony Moutte <instabledesign@gmail.com>
2019-05-28 08:46:04 +0200

    fixup! Add Gelf/graylog handler

--- examples/formatter/gelf_test.go
+++ examples/formatter/gelf_test.go
@@ -16,10 +16,10 @@ func ExampleGelfFormatter() {
 	monkey.Patch(os.Hostname, func() (string, error) { return "my_fake_hostname", nil })
 	defer monkey.UnpatchAll()
 
-	gelfFormatter, _ := formatter.NewGelf()
+	gelfFormatter := formatter.NewGelfEncoder()
 
-	fmt.Println(gelfFormatter.Format(logger.Entry{Message: "My log message", Level: logger.InfoLevel, Context: &map[string]interface{}{"my_key": "my_value"}}))
+	fmt.Println(gelfFormatter.Format(&logger.Entry{Message: "My log message", Level: logger.InfoLevel, Context: logger.NewContext().Add("my key", "my_value")}))
 
 	//Output:
-	// {"version":"1.1","host":"my_fake_hostname","short_message":"My log message","full_message":"TODO","timestamp":513216000,"level":6}
+	//{"version":"1.1","host":"my_fake_hostname","level":6,"timestamp":513216000,"short_message":My log message,"full_message":<info> My log message [ <my key:my_value> ],"_my_key":"my_value"}
 }
