36c1d76840d5ca128006ba6e7cba8df608e5ea86
instabledesign <instabledesign@gmail.com>
2019-10-19 15:35:06 +0200

    add compress provider (#51)

---
+++ writer/provider/compress_test.go
@@ -0,0 +1,38 @@
+package provider_test
+
+import (
+	"errors"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/gol4ng/logger/writer"
+	"github.com/gol4ng/logger/writer/provider"
+)
+
+func TestCompressProvider_WithError(t *testing.T) {
+	var writer io.Writer
+	var MyProvider = func(w io.Writer) (io.Writer, error) {
+		assert.Equal(t, writer, w)
+		return w, errors.New("my_fake_provider_error")
+	}
+	w := provider.CompressProvider(MyProvider)
+	file, err := w(writer)
+	assert.Nil(t, file)
+	assert.EqualError(t, err, "my_fake_provider_error")
+}
+
+func TestCompressProvider(t *testing.T) {
+	createdFile := &os.File{}
+	var MyProvider = func(w io.Writer) (io.Writer, error) {
+		assert.Nil(t, w)
+		return createdFile, nil
+	}
+
+	w := provider.CompressProvider(MyProvider)
+	newFile, err := w(nil)
+	assert.Nil(t, err)
+	assert.IsType(t, &writer.CompressWriter{}, newFile)
+}
