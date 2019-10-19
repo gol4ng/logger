e55ba9691e8b0655429399321d8066f15b24cf50
instabledesign <instabledesign@gmail.com>
2019-10-19 14:54:15 +0200

    add compress writer options (#50)

--- writer/compress.go
+++ writer/compress.go
@@ -18,6 +18,23 @@ const (
 	CompressNone
 )
 
+// CompressOption is compress writer option setter function
+type CompressOption func(*CompressWriter)
+
+// CompressionType set compression type on compress writer
+func CompressionType(t CompressType) CompressOption {
+	return func(w *CompressWriter) {
+		w.compressionType = t
+	}
+}
+
+// CompressionLevel set compression level on compress writer
+func CompressionLevel(level int) CompressOption {
+	return func(w *CompressWriter) {
+		w.compressionLevel = level
+	}
+}
+
 // CompressWriter will decorate io.Writer in order to compress the data
 type CompressWriter struct {
 	io.Writer
@@ -56,8 +73,12 @@ func (w *CompressWriter) Write(p []byte) (int, error) {
 }
 
 // NewCompressWriter will return a new compress writer
-func NewCompressWriter(writer io.Writer, compressionType CompressType, compressionLevel int) *CompressWriter {
-	return &CompressWriter{Writer: writer, compressionType: compressionType, compressionLevel: compressionLevel}
+func NewCompressWriter(writer io.Writer, options ...CompressOption) *CompressWriter {
+	w := &CompressWriter{Writer: writer, compressionType: CompressNone}
+	for _, option := range options {
+		option(w)
+	}
+	return w
 }
 
 // 1k bytes buffer by default
