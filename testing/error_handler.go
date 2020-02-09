fe1fb2b6b5b3fe1a89e5d8e5d0f4d6b4d8440917
Anthony Moutte <instabledesign@gmail.com>
2020-02-09 12:09:19 +0100

    fixup! Improve logger signature error handling

--- testing/error_handler.go
+++ testing/error_handler.go
@@ -1,13 +1,11 @@
 package testing
 
 import (
-	"testing"
-
 	"github.com/gol4ng/logger"
 	"github.com/stretchr/testify/assert"
 )
 
-func AssertErrorHandlerNotCalled(t *testing.T) func(err error, entry logger.Entry) {
+func AssertErrorHandlerNotCalled(t assert.TestingT) func(err error, entry logger.Entry) {
 	return func(err error, entry logger.Entry) {
 		assert.Fail(t, "ErrorHandler must not be called", "unexpected error \"%s\" occurred for %#v", err, entry)
 	}
