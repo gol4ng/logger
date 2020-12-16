dd3b592b1be2d53a97417d25387e8a85388fbc10
Anthony Moutte <instabledesign@gmail.com>
2020-12-16 12:25:49 +0100

    fixup context key collision

--- ctx.go
+++ ctx.go
@@ -2,16 +2,20 @@ package logger
 
 import "context"
 
-var contextKey struct{}
+type contextKey int
+
+const (
+	loggerKey contextKey = iota
+)
 
 // InjectInContext will inject a logger into the go-context
 func InjectInContext(ctx context.Context, l LoggerInterface) context.Context {
-	return context.WithValue(ctx, contextKey, l)
+	return context.WithValue(ctx, loggerKey, l)
 }
 
 // FromContext will retrieve a logger from the go-context or return defaultLogger
 func FromContext(ctx context.Context, defaultLogger LoggerInterface) LoggerInterface {
-	if _logger, ok := ctx.Value(contextKey).(LoggerInterface); ok {
+	if _logger, ok := ctx.Value(loggerKey).(LoggerInterface); ok {
 		return _logger
 	}
 	return defaultLogger
